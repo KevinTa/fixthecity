bf7ab644a0ee6a649c804249d208c38154bf4af5
KevinTa <loneshark26@yahoo.com>
2017-04-03 11:31:57 -0700

    Finalvers

--- mobile/mobile.js
+++ mobile/mobile.js
@@ -1,109 +1,282 @@
-List = new Mongo.Collection("list");
+List = new Mongo.Collection("list");//List Collection to hold entered data
 
 if (Meteor.isClient) {
-  // This code only runs on the client
-  Meteor.startup(function () {
+var MAP_ZOOM = 15;
 
-    sAlert.config({
-        effect: '',
-        position: 'top-right',
-        timeout: 5000,
-        html: false,
-        onRouteClose: true,
-        stack: true,
-        // or you can pass an object:
-        // stack: {
-        //     spacing: 10 // in px
-        //     limit: 3 // when fourth alert appears all previous ones are cleared
-        // }
-        offset: 0, // in px - will be added to first alert (bottom or top - depends of the position in config)
-        beep: false,
-        // examples:
-        // beep: '/beep.mp3'  // or you can pass an object:
-        // beep: {
-        //     info: '/beep-info.mp3',
-        //     error: '/beep-error.mp3',
-        //     success: '/beep-success.mp3',
-        //     warning: '/beep-warning.mp3'
-        // }
-        onClose: _.noop //
-        // examples:
-        // onClose: function() {
-        //     /* Code here will be executed once the alert closes. */
-        // }
+/*functions to run at startup*/
+Meteor.startup(function () {
+/*Loads Google Maps and starts 'showMainMenu' session*/
+  GoogleMaps.load();
+  Session.set("showMainMenu", 1);
+  Session.set("showPhoto", 0);
+  Session.set("showCategories", 0);
+  Session.set("showTopic", 0);
+  Session.set("showDetails", 0);
+  Session.set("showConfirmation", 0);
+
+  sAlert.config({
+    effect: '',
+    position: 'top-right',
+    timeout: 5000,
+    html: false,
+    onRouteClose: true,
+    stack: true,
+    offset: 0, // in px - will be added to first alert (bottom or top - depends of the position in config)
+    beep: false,
+    onClose: _.noop //
+  });
+
+});
+
+/*Event handlers for 'MainMenu' session*/
+Template.MainMenu.events({
+
+  /*After clicking 'submit',active session is now 'showPhoto'*/
+  'click .submitareport': function(event) {
+    event.preventDefault();
+    Session.set("showMainMenu", 0);
+	 Session.set("showPhoto", 1);
+  }
+});
+
+/*Event handlers for 'PhotoPage' session*/
+Template.PhotoPage.events({
+
+ /*Clicking 'take photo' will cause the camera to take
+ a photograph*/
+ 'click .takePhoto': function(event, template) {
+    var cameraOptions = {
+      width: 800,
+      height: 600
+    };
+    MeteorCamera.getPicture(cameraOptions, function (error, data) {
+      if (error) {
+        var warining=sAlert.warning('error with picture', {timeout: 'none'});
+      }
+      else{
+        $('.photo').show();
+        $('.photo').attr('src', data); 
+        $('.takePhoto').hide();
+        $('.retakePhoto').show();
+        $('.next').show();
+        $('label').hide();
+      }  
     });
+    event.preventDefault();
+  },
+  /*clicking 'next' submits the photo, and starts the 'showCategories' session*/
+  'click .next': function(event) {
+    event.preventDefault(); 
+    var PictureSrcSubmit= $( '.photo' ).attr('src') ;
+    Session.set("picSubmit", PictureSrcSubmit);
+    Session.set("showPhoto", 0);
+    Session.set("showCategories", 1);
+  }   
+});
 
-});;
-  Template.example.events({
-    'click .takePhoto': function(event, template) {
-        var cameraOptions = {
-            width: 800,
-            height: 600
-        };
-        MeteorCamera.getPicture(cameraOptions, function (error, data) {
-            if (error) {
-              var warining=sAlert.warning('error with picture', {timeout: 'none'});
-            }
-              else{
-           $('.photo').show();
-           $('.photo').attr('src', data); 
-         }  
-        });
-        event.preventDefault();
+/*Event handlers for Category Page*/
+Template.CategoryPage.events({ 
+
+  /*clicking 'next' will set variable 'category'
+  to selected category, then start 'showGPS' session*/
+  'click .next': function(event) {
+    event.preventDefault();
+    var elements = document.getElementsByName("category");//start of getting selecting category
+
+      for (var i = 0; i < elements.length; i++){
+        if (elements[i].checked)
+        {
+          var category = elements[i].value;
+        }
+      }
+    Session.set("categorySubmit", category);
+    Session.set("topicSubmit", category);
+    Session.set("showCategories", 0);
+    Session.set("showGPS", 1);
+  }
+});
+
+
+/*Event handlers for 'GPSPage'*/
+Template.GPSPage.events({
+  
+  /*Clicking 'next' start 'showDetail' session*/
+   'click .next': function(event) {
+      event.preventDefault();
+
+      Session.set("showGPS", 0);
+      Session.set("showDetails", 1);
     }
+});
+
+/*Shows Google Map*/
+Template.map.onCreated(function() {
+    var self = this;
+
+    GoogleMaps.ready('map', function(map) {
+      var marker;
+
+      // Create and move the marker when latLng changes.
+      self.autorun(function() {
+       
+        var latLng = Geolocation.latLng();
+        if (! latLng){
+          
+          return;
+        }
+
+        // If the marker doesn't yet exist, create it.
+        if (! marker) {
+          marker = new google.maps.Marker({
+            position: new google.maps.LatLng(latLng.lat, latLng.lng),
+            map: map.instance
+          });
+          Session.set("latitudeSubmit", latLng.lat);
+          Session.set("longitudeSubmit", latLng.lng);
+        }
+        // The marker already exists, so we'll just change its position.
+        else {
+          marker.setPosition(latLng);
+        }
+
+        // Center and zoom the map view onto the current position.
+        map.instance.setCenter(marker.getPosition());
+        map.instance.setZoom(MAP_ZOOM);
+      });
+    });
   });
   
+/*Helper functions for 'map*/
+Template.map.helpers({
 
-    Template.body.events({
-    "submit .new-task": function (event) {
-      // Prevent default browser form submit
+	/*Returns error message in case of an error*/
+    geolocationError: function() {
+      var error = Geolocation.error();
+      return error && error.message;
+    },
+	
+	/*Initializes map and centers in and zooms on location*/
+    mapOptions: function() {
+      var latLng = Geolocation.latLng();
+      // Initialize the map once we have the latLng.
+      if (GoogleMaps.loaded() && latLng) {
+        return {
+          center: new google.maps.LatLng(latLng.lat, latLng.lng),
+          zoom: MAP_ZOOM
+        };
+      }
+    }
+  });
+/*Helper functions for Confirmation page;
+Each one returns the data entered from 
+each session*/
+Template.ConfirmationPage.helpers({
+  image: function(){
+    return Session.get("picSubmit");
+  },
+  category: function(){
+    return Session.get("categorySubmit");
+  },
+  topic: function(){
+    return Session.get("topicSubmit");
+  },
+  latitude: function(){
+    return Session.get("latitudeSubmit");
+  },
+  longitude: function(){
+    return Session.get("longitudeSubmit");
+  }
+});
+
+/*Event handlers for 'TopicPage'*/
+Template.TopicPage.events({
+
+	/*Clicking 'next' start 'showGPS' session*/
+   'click .next': function(event) {
       event.preventDefault();
+      var topic= event.delegateTarget.topic.value;
+      Session.set("topicSubmit",topic);
+      Session.set("showTopic", 0);
+      Session.set("showGPS", 1);
+    }
+});
+
+/*Event handlers for 'OptionalSetailsPage'*/
+Template.OptionalDetailsPage.events({
+
+    /*clicking 'next' starts 'showConfirmation' session*/
+  'click .next': function(event) {
+    event.preventDefault();
+    var details= event.delegateTarget.details.value;
+    var name= event.delegateTarget.username.value;
+    Session.set("detailsSubmit",details);
+
+    Session.set("nameSubmit",name);
+    Session.set("showDetails", 0);
+    Session.set("showConfirmation", 1);
+  }
+});
+
+/*Event handlers for 'formhere'*/
+Template.formHere.events({
+
+  /*assigns variables to data entered from sessions*/
+  "submit form": function (event) {
+      // Prevent default browser form submit
+    event.preventDefault();
+    
       // Get value from form element
-      var topic= event.target.topic.value;
-      var details = event.target.details.value;
-	  var src=  $( '.photo' ).attr('src');
-      var hidden="hidden";
-	  var longitude = event.target.longitude.value;//longitude inserted for testing purposes
-	  var latitude = event.target.latitude.value;//latitude inserted for testing purposes
-	  var elements = document.getElementsByName("category");//start of getting selecting category
-		{
-			for (var i = 0; i < elements.length; i++)
-			if (elements[i].checked)
-			{
-				var category = elements[i].value;
-			}
-		}//end of selecting category
-		var heading = category+longitude+latitude;//heading is combination of category and location to distinguish each row in the table
-	  if(/^([0-9a-zA-Z]+)/.test(event.target.topic.value)==true && /^([0-9a-zA-Z]+)/.test(event.target.details.value)==true)//regex check for blank text boxes
-		{
-		// Insert a task into the collection
-		List.insert({
-			category: category,
-			topic: topic,
-			details: details,
-			longitude: longitude,
-			latitude: latitude,
-			heading: heading,
-			createdAt: new Date() // current time
-		});
-		// Clear form
-		event.target.topic.value = "";
-		event.target.details.value = "";
-		event.target.longitude.value = "";
-		event.target.latitude.value = "";
-		}
-	else
-		{
-		alert("Enter something in the boxes");
-		}
+    var topic= Session.get("topicSubmit");
+    var details = Session.get("detailsSubmit");
+    var src=  Session.get("picSubmit");
+    var category= Session.get("categorySubmit");
+    var latitude= Session.get("latitudeSubmit");
+    var longitude= Session.get("longitudeSubmit");
+    var hidden="hidden";
+    
+    
+    //end of selecting category
+    var heading = category+longitude+latitude;//heading is combination of category and location to distinguish each row in the table
+   
+    /*Insert variables into Collection*/
+      List.insert({
+        category: category,
+        topic: topic,
+        details: details,
+        image: src,
+        longitude: longitude,
+        latitude: latitude,
+        heading: heading,
+        createdAt: new Date() // current time
+      });
+    Session.set("showConfirmation", 0);
+    Session.set("showMainMenu", 1);
+    
+
+	  
 	}
-  });
+});
+
+
+/*Returns current session*/
+Template.registerHelper('session', function( value ) {
+  return Session.get(value);
+});
+
+/*must bind to `document.body` as element will be replaced during re-renders
+add the namespace `.tplquestions` so all event handlers can be removed easily*/
+Template.CategoryPage.created = function(){
+  $(document.body).on('change.tplquestions', '.form', function(e){
+   
+     $('.next').show();
+  }); 
+};
+
+/*remove all event handlers in the namespace `.tplquestions`*/
+Template.CategoryPage.destroyed = function(){
+  $(document.body).off('.tplquestions');
 }
 
 
-
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
 }
+
--- web/web.js
+++ web/web.js
@@ -1,74 +1,119 @@
-List = new Mongo.Collection("list");
+List = new Mongo.Collection("list"); //List Collection to hold all entries
+Display = new Mongo.Collection(null); //Display Collection tol hold entries to be displayed
 var MAP_ZOOM = 15;
 if (Meteor.isClient) {
   // This code only runs on the client
-  /*Template.body.helpers({
+  var current; //Variable keeps track of clicked entry in reactive table
+  
+/*Helper functions for homepage*/
+  Template.home.helpers({
+  
+	/*Scans List Collection for entries with distinct topics,
+	counts number of entries for each distinct topic,
+	then inserts this information into Display Collection*/
     list: function(){
-	var myArray = List.find({}, {sort: {createdAt: -1}}).fetch();
-	var distinctHead = _.uniq(myArray, true, function(List) {return List.heading});
-	return distinctHead;
-	}
-  });*/
-  Template.home.helpers({//helper for home template to show all distinct issues
-  list: function(){
-	var myArray = List.find({}, {sort: {createdAt: -1}}).fetch();
-	var distinctHead = _.uniq(myArray, true, function(List) {return List.heading});
-	return distinctHead;
-	}
-  });
-  Template.entry.helpers({
-  quantity: function(entry){
-  var q=0;
-  var myArray = List.find({}, {sort: {createdAt: -1}}).fetch();
-  var headings = function(myArray){return _.pluck(myArray, "heading")};
-  var headarray = headings(myArray);
-  for(var i = 0; i<headarray.length; i++)
-  {
-	if(this.heading==headarray[i])
-	{
-		q++;
-	}
-  }
-  return q;
-  }
-  });
-  Template.show.helpers({//helper for show template to only show issues of the same category and location 
-	list: function(){
-	var myArray = List.find({}, {sort: {createdAt: -1}}).fetch();
-	var cy=Session.get('currentiss');
-	var nlist=new Array();
-	for(var i = 0; i<myArray.length;i++)
-	{
-		if(myArray[i].heading==cy)
-		{
-			nlist.push(myArray[i]);
-		}
-	}
-	return nlist;
-	}
-  });
-  Template.entry.events({
-    "click .delete": function () {
-      List.remove(this._id);
+    Display.remove({});
+    var myArray = List.find({}, {sort:{createdAt: -1}}).fetch();
+    var distinctHead = _.uniq(myArray, false, function(List) {return List.topic});
+    var valuearray= new Array();
+    var topics = function(myArray){return _.pluck(distinctHead, "topic")};
+    var topicarray = topics(distinctHead);
+    for(var i = 0; i<topicarray.length; i++)
+    {
+         valuearray[i]=0;
+      for(var j = 0; j<myArray.length; j++)
+      {
+        if(topicarray[i]==myArray[j].topic)
+        {
+          valuearray[i]++;
+        }
+      }
+    }
+    for(var k =0;k<distinctHead.length;k++)
+    {
+      distinctHead[k].quantity=valuearray[k];
+    }
+    for(var m =0;m<distinctHead.length; m++){
+    Display.insert(distinctHead[m]);
+        }   
     },
-	"click .iss": function(e){//new click function to get the heading of the clicked element
-	 currentiss=Session.set('currentiss', this.heading);
-	}
+	
+	/*Returns Display Collection with the 
+	fields: key for pulling the data from the collection and
+	label for the corresponding header to be displayed in the
+	reactive table in web.HTML*/
+    settings: function () {
+      return {
+        collection: Display,
+        fields: [
+          {key: 'topic', label: 'Category'},
+          {key: 'details', label: 'Description'},
+          {key: 'createdAt', label: 'Date Added'},
+          {key: 'quantity', label: 'Quantity'},
+        ]
+      };
+    }
   });
-  	
+  
+/*Helper functions for the page that shows entries of clicked topics*/
+  Template.show.helpers({
+  
+  /*Scans List Collection for entries that match topics with clicked entry
+  in reactive table*/
+    list: function(){
+      var myArray = List.find({}, {sort:{createdAt: -1}}).fetch();
+      var nlist= new Array();
+      for(var i = 0; i< myArray.length;i++)
+      {
+        if(myArray[i].topic==current)
+        {
+          nlist.push(myArray[i]);
+        }
+      }
+      return nlist;
+    }
+
+  });
+  
+  /*Event handlers the page that shows entries of clicked topics*/
+  Template.show.events({
+  
+    /*Routes to home page when user clicks back button with class 'btn-secondary' on 'show' page*/
+    "click .btn-secondary": function(){
+      Router.go('/');
+    },
+	
+	/*Deletes object from List Collection, when icon with class 'delete' is clicked*/
+    "click .delete": function(){
+      List.remove(this._id);
+    }
+    });
+  
+  /*Event handlers for home page*/
+  Template.home.events({
+  
+  /*Sets variable 'current' to topic of clicked entry in reactive table,'
+  then routes user to 'show' page*/
+  'click .reactive-table tbody tr': function () {
+   current = this.topic;
+   Router.go('/show');
+    }
+  });
 }
-//iron router tied to home and show templates
-Router.route('/', {
-	name: 'home',
-    template: 'home'
-});
-Router.route('/show',{
-	name:'show',
-	template: 'show'
+
+/*Routes user to 'show' page, which displays all entries
+of matching topics to clicked entry*/
+Router.route('/show', {
+  name:'show',
+  template:'show'
 });
 
+/*Routes user to home page*/
+Router.route('/', {
+  name:'home',
+  template:'home'
+});
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    // code to run on server at startup
   });
 }
